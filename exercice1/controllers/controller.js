d0894a69ca632cbe99808d2666c352b37b4cc27b
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-05-15 18:19:43 +0100

    first-commit

---
+++ exercice1/controllers/controller.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+import { randomUUID } from 'crypto';    
+
+const datapath = path.resolve("database/data.json");
+
+const controller = {}
+
+export default controller;

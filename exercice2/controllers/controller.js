cfc229197abd99b62d7f5a1f7078d1f77619f2de
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-05-19 12:53:00 +0100

    updateContact

--- exercice2/controllers/controller.js
+++ exercice2/controllers/controller.js
@@ -33,6 +33,65 @@ const contactController = {
                 res.status(201).json("Contact ajouté avec succès");
             });
         });
+    },
+
+    getAllContacts: (req, res) => {
+        fs.readFile(jsonpath, "utf-8", (err, data) => {
+            if (err) return res.status(500).json("Erreur de lecture du fichier");
+            const contacts = data ? JSON.parse(data) : [];
+
+            let csv = "id,nom,email,phone\n";
+            contacts.forEach((contact) => {
+                csv += `${contact.id},${contact.nom},${contact.email},${contact.phone}\n`;
+            });
+
+            fs.writeFile(csvpath, csv, "utf-8", (err) => {
+                if (err) console.log("Erreur écriture csv");
+            });
+            res.json(contacts);
+        });
+    },
+
+    getContactById: (req, res) => {
+        const id = req.params.id;
+        fs.readFile(jsonpath, "utf-8", (err, data) => {
+            if (err) return res.status(500).json("Erreur de lecture du fichier");
+            const contacts = data ? JSON.parse(data) : [];
+            const contact = contacts.find((c) => c.id === id);
+            if (!contact) return res.status(404).json("Contact non trouvé");
+            res.status(200).json(contact);
+        });
+    },
+
+    updateContact: (req, res) => {
+        const { nom, email, phone } = req.body;
+        const id = req.params.id;
+
+        fs.readFile(jsonpath, "utf-8", (err, data) => {
+            if (err) return res.status(500).json("Erreur de lecture du fichier");
+            const contacts = data ? JSON.parse(data) : [];
+            const contact = contacts.find((c) => c.id === id);
+
+            if (!contacts) return res.status(404).json("Contact non trouvé");
+
+            if (nom) contact.nom = nom;
+            if (email) contact.email = email;
+            if (phone) contact.phone = phone;
+
+            fs.writeFile(jsonpath, JSON.stringify(contacts), "utf-8", (err) => {
+                if (err) return res.status(500).send("Erreur écriture JSON");
+
+                let csv = "id,nom,email,phone\n";
+                contacts.forEach((contact) => {
+                    csv += `${contact.id},${contact.nom},${contact.email},${contact.phone}\n`;
+                });
+
+                fs.writeFile(csvpath, csv, "utf-8", (err) => {
+                    if (err) console.log("Erreur écriture csv");
+                    res.status(200).json("Contact mis à jour avec succès");
+                });
+            });
+        });
     }
 }
 
--- exercice2/routes/route.js
+++ exercice2/routes/route.js
@@ -4,10 +4,10 @@ import contactController from "../controllers/controller.js";
 const router = Router();
 
 router.post("/contacts", contactController.createContact);
-/* router.get("/contacts", contactController.getAllContacts);
+router.get("/contacts", contactController.getAllContacts);
 router.get("/contacts/:id", contactController.getContactById);
 router.put("/contacts/:id", contactController.updateContact);
-router.delete("/contacts/:id", contactController.deleteContact);
+/* router.delete("/contacts/:id", contactController.deleteContact);
 router.get("/status", contactController.getStatus); */
 
 export default router;
